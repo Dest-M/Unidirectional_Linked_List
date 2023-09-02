e77447f8dc1d7990678c61198456f07d0f961df0
hp <hp@LAPTOP-QLT1H0K9>
2023-09-02 20:07:03 +0300

    Add project files.

---
+++ Unidirectional_Linked_List/List_Class.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Unidirectional_Linked_List
+{
+    internal class List
+    {
+        int val;
+        List next;
+        public List(int val)
+        {
+            this.val = val;
+        }
+        public void add(int val)
+        {
+            List tempNext = this;
+            while (tempNext.next != null)
+            {
+                tempNext = tempNext.next;
+            }
+            if (tempNext.next == null)
+            {
+                tempNext.next = new List(val);
+            }
+
+        }
+        public void remove()
+        {
+            List tempNext = this;
+            List prev = tempNext;
+            while (tempNext.next != null)
+            {
+                prev = tempNext;
+                if (tempNext.next != null)
+                {
+                    tempNext = tempNext.next;
+                }
+            }
+            prev.next = null;
+
+        }
+        public void display()
+        {
+            List virnext = this;
+            while (virnext != null)
+            {
+                Console.Write(Convert.ToString(virnext.val) + " ");
+                virnext = virnext.next;
+            }
+        }
+    }
+}
+
+
---
+++ Unidirectional_Linked_List/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Unidirectional_Linked_List
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List l = new List(2);
+            l.add(3);
+            l.add(4);
+            l.add(5);
+            l.add(6);
+            l.remove();
+            l.add(7);
+
+            l.display();
+        }
+
+    }
+}
